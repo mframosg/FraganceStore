36d0a1a749179698598abe25195dc204967e5c6e
Miguel Ramos Garcia <mframosg@eafit.edu.co>
2021-09-25 19:03:55 -0500

    card sizing fixed in admin index

--- resources/views/admin/admin-index.blade.php
+++ resources/views/admin/admin-index.blade.php
@@ -1,30 +1,33 @@
 @extends('layouts.app') @section('content')
-@section('user',auth()->user()->getName())
-    <h2 class=" text-center col-12">Available fragrances list</h2>
-    @foreach ($fragances as $fragance)
-        <div class="card-group">
-            <div class="card">
+@section('user',
+    auth()->user()->getName(),)
+    <h2 class=" text-center mt-5 mb-5 col-12">Available fragrances list</h2>
+    
+        <div class="card-columns">
+            @foreach ($fragances as $fragance)
+                <div class="card">
 
-                <img src="{{ asset("/img/fragance/$fragance->image") }}" class="d-block col-8" alt="not found">
+                    <img src="{{ asset("/img/fragance/$fragance->image") }}" class="card-img-top" alt="not found">
 
-            <div class="card-body">
-                <h5 class="card-title"><a style="text-decoration: none"
-                        href="{{ route('fragance.show', $fragance->getId()) }}">
-                        {{ $fragance->getTitle() }}</a></h5>
-                <p class="card-text">{{ $fragance->getDescription() }}</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted">{{ $fragance->getCategory() }}</small>
-                <small class="text-muted">{{ $fragance->getPrice() }}</small>
-            </div>
+                    <div class="card-body">
+                        <h5 class="card-title"><a style="text-decoration: none"
+                                href="{{ route('fragance.show', $fragance->getId()) }}">
+                                {{ $fragance->getTitle() }}</a></h5>
+                        <p class="card-text">{{ $fragance->getDescription() }}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">{{ $fragance->getCategory() }}</small>
+                        <small class="text-muted">{{ $fragance->getPrice() }}</small>
+                    </div>
+                </div>
+            @endforeach
         </div>
-        </div>
 
-    @endforeach
-
-    <div class="text-center">
-        <a href="{{ route('fragance.index') }}" class="btn btn-primary">
-            Agregar
+    
+    
+    <div class="text-center mt-5 mb-5">
+        <a href="{{ route('fragance.index') }}" class="btn-lg btn-primary p-3">
+            Add another fragance
         </a>
     </div>
 @endsection
