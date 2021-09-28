d4f0cdf6e1196295c92553c4b82ce26e59ca53b8
Cristian Alexis Giraldo Agudelo <cagiraldoa@eafit.edu.co>
2021-09-28 03:47:10 -0500

    s 3

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('home/{id}', 'App\Http\Controllers\WishListController@add')->name("wishlist.add");
+//Route::get('home/{id}', 'App\Http\Controllers\WishListController@add')->name("wishlist.add");
 
-Route::delete('home/{id}', 'App\Http\Controllers\WishListController@delete')->name("wishlist.delete");
+//Route::delete('home/{id}', 'App\Http\Controllers\WishListController@delete')->name("wishlist.delete");
 
 Route::get('/home/car', 'App\Http\Controllers\HomeController@buy')->name('car.buy')->middleware('auth');
 
