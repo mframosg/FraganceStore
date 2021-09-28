b3d3aacad9b44dd1058ad36bcf4d3b6bc384a781
Cristian Alexis Giraldo Agudelo <cagiraldoa@eafit.edu.co>
2021-09-28 10:43:38 -0500

    final 11

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,35 +1,31 @@
 @extends('layouts.app') @section('content')
-
-    <!-- Portfolio Section-->
-    <section class="page-section portfolio" id="portfolio">
-        <div class="container">
-            <!-- Portfolio Section Heading-->
-            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-5">{{ __('home.List') }}</h2>
-            <!-- Portfolio Grid Items-->
-            <div class="row">
-                <!-- Portfolio Item 1-->
-                @foreach ($fragances as $fragance)
-                    <div class="col-md-6 col-lg-4 mb-5">
-                        <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
-                            <a style="text-decoration: none" href="{{ route('fragance.info', $fragance->getId()) }}">
-                                <div
-                                    class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-
-                                    <div class="portfolio-item-caption-content text-center text-white">
-                                        <i class="fas fa-plus fa-3x"></i>
-                                    </div>
-                                </div>
-                            </a>
-                            <img class="img-fluid" src="{{ asset("/img/fragance/$fragance->image") }}"
-                                alt="not founded" />
-                        </div>
-                    </div>
-                @endforeach
-            </div>
+<!-- Portfolio Section-->
+<section class="page-section portfolio" id="portfolio">
+  <div class="container">
+    <!-- Portfolio Section Heading-->
+    <h2 class="page-section-heading text-center text-uppercase text-secondary mb-5">{{ __('home.List')}}</h2>
+    <!-- Portfolio Grid Items-->
+    <div class="row">
+      <!-- Portfolio Item 1-->
+      @foreach ($fragances as $fragance)
+      <div class="col-md-6 col-lg-4 mb-5">
+        <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+          <a style="text-decoration: none" href="{{ route('fragance.info', $fragance->getId()) }}">
+          <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+            
+              <div class="portfolio-item-caption-content text-center text-white">
+                <i class="fas fa-plus fa-3x"></i>
+              </div>
+          </div>
+        </a>
+          <img class="img-fluid" src="{{ asset("/img/fragance/$fragance->image") }}" alt="not founded" />
         </div>
-    </section>
-    @if (auth()->user())
-        @section('user',
-            auth()->user()->getName(),)
-        @endif
-    @endsection
+      </div>
+      @endforeach
+    </div>
+  </div>
+</section>
+@if(auth()->user())
+  @section('user', auth()->user()->getName(),)
+@endif
+@endsection
