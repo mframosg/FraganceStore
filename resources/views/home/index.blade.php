82e8f98eb9c515b75c02f30d2a134e1d0e8a63b7
afernander <afernander@eafit.edu.co>
2021-11-15 19:50:24 -0500

    en added

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 <section class="page-section portfolio" id="portfolio">
   <div class="container">
     <!-- Portfolio Section Heading-->
-    <h2 class="page-section-heading text-center text-uppercase text-secondary mb-5">Fragances</h2>
+    <h2 class="page-section-heading text-center text-uppercase text-secondary mb-5">{{ __(home.fragances) }}</h2>
     <!-- Portfolio Grid Items-->
     <div class="row">
       <!-- Portfolio Item 1-->
